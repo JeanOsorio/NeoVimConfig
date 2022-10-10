9e82775eb856526043a9f59b4795d3505727be65
Jean Osorio <josorioe@gmail.com>
2022-10-10 06:58:41 +0200

    add status bar

--- init.lua
+++ init.lua
@@ -1,16 +1,17 @@
-require "custom.options"
-require "custom.keymaps"
-require "custom.plugins"
-require "custom.colorscheme"
-require "custom.cmp"
-require "custom.nvimtree"
-require "custom.lsp"
-require "custom.mason"
-require "custom.telescope"
-require "custom.null-ls"
-require "custom.treesitter"
-require "custom.autopairs"
-require "custom.comment"
+require("custom.options")
+require("custom.keymaps")
+require("custom.plugins")
+require("custom.colorscheme")
+require("custom.cmp")
+require("custom.nvimtree")
+require("custom.lsp")
+require("custom.mason")
+require("custom.telescope")
+require("custom.null-ls")
+require("custom.treesitter")
+require("custom.autopairs")
+require("custom.comment")
+require("custom.feline")
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
