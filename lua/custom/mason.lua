19f8d33cfe188cb56554091d532b06d28e87fa75
Jean Osorio <josorioe@gmail.com>
2022-10-12 10:53:40 +0200

    remove denols

--- lua/custom/mason.lua
+++ lua/custom/mason.lua
@@ -1,14 +1,14 @@
 require("mason").setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗"
-    }
-  }
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
 require("mason-lspconfig").setup({
-  ensure_installed = { "sumneko_lua", "tsserver", "jsonls", "emmet_ls", "stylelint_lsp", "solidity", "solhint", "denols" },
-  automatic_installation = true,
+	ensure_installed = { "sumneko_lua", "tsserver", "jsonls", "emmet_ls", "stylelint_lsp", "solidity", "solhint" },
+	automatic_installation = true,
 })
