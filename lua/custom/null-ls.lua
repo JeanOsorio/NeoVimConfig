4fccca4683a2f6354ed1e201cf3330c4054ae07c
Jean Osorio <JeanOsorio@users.noreply.github.com>
2023-01-20 14:38:00 +0100

    fix: format on save

--- lua/custom/null-ls.lua
+++ lua/custom/null-ls.lua
@@ -1,33 +1,42 @@
 local present, null_ls = pcall(require, "null-ls")
 
 if not present then
-  return
+	return
 end
 
 local b = null_ls.builtins
 
 local sources = {
-  b.diagnostics.eslint,
-  b.diagnostics.stylelint,
-  b.code_actions.gitsigns,
-  b.diagnostics.solhint,
-  -- webdev stuff
-  -- b.formatting.deno_fmt,
-  b.formatting.prettier,
-  b.formatting.jq,
-  -- Lua
-  b.formatting.stylua,
-  -- Shell
-  b.formatting.shfmt,
-  b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
+	b.diagnostics.eslint,
+	b.diagnostics.stylelint,
+	b.code_actions.gitsigns,
+	b.diagnostics.solhint,
+	-- webdev stuff
+	-- b.formatting.deno_fmt,
+	b.formatting.prettier,
+	b.formatting.jq,
+	-- Lua
+	b.formatting.stylua,
+	-- Shell
+	b.formatting.shfmt,
+	b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
-  debug = false,
-  sources = sources,
-  on_attach = function(client)
-    if client.server_capabilities.document_formatting then
-      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-    end
-  end,
+	debug = false,
+	sources = sources,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
