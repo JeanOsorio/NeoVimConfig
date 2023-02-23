54cbfbf5215b4273519219c61edb58f25e1cdef7
Jean Osorio <JeanOsorio@users.noreply.github.com>
2023-02-23 09:39:34 +0100

    feat: add treesitter context

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+  PACKER_BOOTSTRAP = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Installing packer close and reopen Neovim...")
+  vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,136 +26,140 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	return
+  return
 end
 
 -- Have packer use a popup window
 packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
 })
 
 -- Install your plugins here
 return packer.startup(function(use)
-	-- My plugins here
-	use("wbthomason/packer.nvim") -- Have packer manage itself
-	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
-	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
-	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
-	use("numToStr/Comment.nvim") -- Easily comment stuff
-	-- nvim-tree to explorer files
-	use({
-		"kyazdani42/nvim-tree.lua",
-		requires = {
-			"kyazdani42/nvim-web-devicons", -- optional, for file icons
-		},
-		tag = "nightly", -- optional, updated every week. (see issue #1193)
-	})
+  -- My plugins here
+  use("wbthomason/packer.nvim") -- Have packer manage itself
+  use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+  use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
+  use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
+  use("numToStr/Comment.nvim") -- Easily comment stuff
+  -- nvim-tree to explorer files
+  use({
+    "kyazdani42/nvim-tree.lua",
+    requires = {
+      "kyazdani42/nvim-web-devicons", -- optional, for file icons
+    },
+    tag = "nightly", -- optional, updated every week. (see issue #1193)
+  })
 
-	-- colorschema themes
-	use("lunarvim/colorschemes")
-	use("ellisonleao/gruvbox.nvim")
-	use("folke/tokyonight.nvim")
+  -- colorschema themes
+  use("lunarvim/colorschemes")
+  use("ellisonleao/gruvbox.nvim")
+  use("folke/tokyonight.nvim")
 
-	-- cmp plugins
-	use("hrsh7th/nvim-cmp") -- The completion plugin
-	use("hrsh7th/cmp-buffer") -- buffer completions
-	use("hrsh7th/cmp-path") -- path completions
-	use("hrsh7th/cmp-cmdline") -- cmdline completions
-	use("saadparwaiz1/cmp_luasnip") -- snippet completions
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-nvim-lua")
+  -- cmp plugins
+  use("hrsh7th/nvim-cmp") -- The completion plugin
+  use("hrsh7th/cmp-buffer") -- buffer completions
+  use("hrsh7th/cmp-path") -- path completions
+  use("hrsh7th/cmp-cmdline") -- cmdline completions
+  use("saadparwaiz1/cmp_luasnip") -- snippet completions
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-nvim-lua")
 
-	-- snippets
-	use("L3MON4D3/LuaSnip") --snippet engine
-	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+  -- snippets
+  use("L3MON4D3/LuaSnip") --snippet engine
+  use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
-	-- LSP
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("neovim/nvim-lspconfig") -- enable
+  -- LSP
+  use("williamboman/mason.nvim")
+  use("williamboman/mason-lspconfig.nvim")
+  use("neovim/nvim-lspconfig") -- enable
 
-	-- Telescope
-	use("nvim-telescope/telescope.nvim")
-	use("nvim-telescope/telescope-media-files.nvim")
+  -- Telescope
+  use("nvim-telescope/telescope.nvim")
+  use("nvim-telescope/telescope-media-files.nvim")
 
-	-- Treesitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	})
+  -- Treesitter
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  })
 
-	-- comments
-	use("JoosepAlviste/nvim-ts-context-commentstring")
-	use({
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	})
+  use({
+    "nvim-treesitter/nvim-treesitter-context",
+  })
 
-	use("p00f/nvim-ts-rainbow")
+  -- comments
+  use("JoosepAlviste/nvim-ts-context-commentstring")
+  use({
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  })
 
-	-- null-ls
-	use("jose-elias-alvarez/null-ls.nvim")
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
+  use("p00f/nvim-ts-rainbow")
 
-	-- status bar plugins
-	use("feline-nvim/feline.nvim")
-	use({
-		"catppuccin/nvim",
-		as = "catppuccin",
-		config = function()
-			vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
-			require("catppuccin").setup()
-			vim.api.nvim_command("colorscheme catppuccin")
-		end,
-	})
+  -- null-ls
+  use("jose-elias-alvarez/null-ls.nvim")
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
 
-	use("lewis6991/gitsigns.nvim")
+  -- status bar plugins
+  use("feline-nvim/feline.nvim")
+  use({
+    "catppuccin/nvim",
+    as = "catppuccin",
+    config = function()
+      vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+      require("catppuccin").setup()
+      vim.api.nvim_command("colorscheme catppuccin")
+    end,
+  })
 
-	-- open tabs
-	use({
-		"kdheepak/tabline.nvim",
-		config = function()
-			require("tabline").setup({
-				-- Defaults configuration options
-				enable = true,
-				options = {
-					-- If lualine is installed tabline will use separators configured in lualine by default.
-					-- These options can be used to override those settings.
-					section_separators = { "", "" },
-					component_separators = { "", "" },
-					max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
-					show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
-					show_devicons = true, -- this shows devicons in buffer section
-					show_bufnr = false, -- this appends [bufnr] to buffer section,
-					show_filename_only = false, -- shows base filename only instead of relative path in filename
-					modified_icon = "+ ", -- change the default modified icon
-					modified_italic = false, -- set to true by default; this determines whether the filename turns italic if modified
-					show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
-				},
-			})
-			vim.cmd([[
+  use("lewis6991/gitsigns.nvim")
+
+  -- open tabs
+  use({
+    "kdheepak/tabline.nvim",
+    config = function()
+      require("tabline").setup({
+        -- Defaults configuration options
+        enable = true,
+        options = {
+          -- If lualine is installed tabline will use separators configured in lualine by default.
+          -- These options can be used to override those settings.
+          section_separators = { "", "" },
+          component_separators = { "", "" },
+          max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
+          show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
+          show_devicons = true, -- this shows devicons in buffer section
+          show_bufnr = false, -- this appends [bufnr] to buffer section,
+          show_filename_only = false, -- shows base filename only instead of relative path in filename
+          modified_icon = "+ ", -- change the default modified icon
+          modified_italic = false, -- set to true by default; this determines whether the filename turns italic if modified
+          show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
+        },
+      })
+      vim.cmd([[
       set guioptions-=e " Use showtabline in gui vim
       set sessionoptions+=tabpages,globals " store tabpages and globals in session
-    ]])
-		end,
-		requires = { { "hoob3rt/lualine.nvim", opt = true }, { "kyazdani42/nvim-web-devicons", opt = true } },
-	})
-	use({
-		"romgrk/barbar.nvim",
-		requires = { "kyazdani42/nvim-web-devicons" },
-	})
+    ]] )
+    end,
+    requires = { { "hoob3rt/lualine.nvim", opt = true }, { "kyazdani42/nvim-web-devicons", opt = true } },
+  })
+  use({
+    "romgrk/barbar.nvim",
+    requires = { "kyazdani42/nvim-web-devicons" },
+  })
 
-	-- gitdiff
-	use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
+  -- gitdiff
+  use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
 
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
