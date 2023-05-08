981467066fcaf6e019ad589e8516ebbcb9e3add8
Jean Osorio <JeanOsorio@users.noreply.github.com>
2023-05-08 10:24:00 +0200

    chore: remove solhit from mason config

--- lua/custom/mason.lua
+++ lua/custom/mason.lua
@@ -1,14 +1,14 @@
 require("mason").setup({
-	ui = {
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
-		},
-	},
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
 })
 
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "tsserver", "jsonls", "emmet_ls", "stylelint_lsp", "solidity", "solhint" },
-	automatic_installation = true,
+  ensure_installed = { "lua_ls", "tsserver", "jsonls", "emmet_ls", "stylelint_lsp", "solidity" },
+  automatic_installation = true,
 })
