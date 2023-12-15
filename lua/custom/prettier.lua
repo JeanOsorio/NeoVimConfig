cea121f455bb911a28bdc2fd4f35c9e1f73b549b
Jean Osorio <josorioe@gmail.com>
2023-12-15 05:09:52 +0100

    config prettier

---
+++ lua/custom/prettier.lua
@@ -0,0 +1,29 @@
+local status_ok, prettier = pcall(require, "prettier")
+if not status_ok then
+  return
+end
+
+prettier.setup({
+  bin = 'prettierd',
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+  mappings = {
+    n = {
+      ["<Leader>"]
+    },
+    v = {},
+  }
+})
+
